560460f9f05c64c4d6a4e037ed1f326f095d863e
Brook1711 <33785776+Brook1711@users.noreply.github.com>
2018-08-02 20:29:59 +0800

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+//作者：brook1711
+//功能：在控制台显示HelloWorld
+//日期：2018/8/1
+//public:表示类是公共的，一个java程序中只能有一个公共类
+//class:表示一个类
+//public class名只能与文件名相同
+public class HelloWorld
+{
+	public static void main(String args[])
+	{
+		System.out.println("Hello World!");
+	}
+}
