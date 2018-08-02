d460529f71217fcf7d7e5237431400d3df346415
brook1711 <670396323@qq.com>
2018-08-02 20:48:48 +0800

    first_commit

---
+++ DivideJudge.java
@@ -0,0 +1,27 @@
+//作者：brook1711
+//功能：判断两个数是否能整除
+//日期：2018/8/2
+public class DivideJudge
+{
+	public static void main(String []arg)
+	{
+		int a=2;
+		int b=4;
+		if(b%a==0)
+		{
+			System.out.println("可以整除");
+		}
+		else
+		{
+			System.out.println("不能整除！");
+		}
+		int c=56;
+		int d=c++;
+		System.out.println("c="+(--c)+";"+"c++="+d);
+		/*
+		int e=3;
+		int f=++(++e)--;
+		System.out.println("++(++3)--="+f);
+		*/
+	}
+}
---
+++ Plus.java
@@ -0,0 +1,29 @@
+//作者：brook1711
+//功能：实现简单加法
+//日期：2018/8/1
+/*
+多
+行
+注释
+*/
+public class Plus
+{
+	public static void main(String args[])
+	{
+		
+		int a=10;
+		int b=20;
+		int result=a+b;
+		System.out.println("结果是："+result);
+		//byte b1=-129;
+		
+		char char1='q';
+		//一个char只能存放一个字符
+		//一个char两个字节，可以存放汉字
+		System.out.println(char1);
+		int test1='a'+'b';
+		int test2='中';
+		System.out.println(test1);
+		System.out.println(test2);
+	}
+}
